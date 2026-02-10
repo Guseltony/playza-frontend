27539a818429bc33e233be94123d69cd84b58b46
Akanji Anthony Guseltony <devguselt@gmail.com>
2026-02-10 21:43:29 +0100

    add app coming notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import NavFooter from "./components/NavFooter";
+import AppNotification from "./components/AppNotification";
 
 const App = () => {
+  const { pathname } = useLocation();
   return (
     <div className="min-h-screen bg-linear-to-b from-gray-900 via-slate-900 to-slate-800 text-white">
       <Header />
+      {pathname === "/" && <AppNotification />}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
